91a4444024c077c97b67dfb5e825bcb403762696
nisehatakiti <nisehatakiti@gmail.com>
2026-09-25 17:40:38 +0900

    Register lighting chapter 5 route

--- wp-content/themes/hatakiti/functions.php
+++ wp-content/themes/hatakiti/functions.php
@@ -132,6 +132,7 @@ function hatakiti_theatre_textbook_route( $template ) {
         'theatre-textbook/staff/lighting/chapter-2',
         'theatre-textbook/staff/lighting/chapter-3',
         'theatre-textbook/staff/lighting/chapter-4',
+        'theatre-textbook/staff/lighting/chapter-5',
         'theatre-textbook/staff/lighting/filters',
         'theatre-textbook/staff/sound',
         'theatre-textbook/staff/stage-management',
